2238ddfbe3736370700c23d68b15c7d0a43ff118
Isaura <isa.ruiz82@gmail.com>
2020-09-06 10:44:46 -0500

    Se implementa el metodo responsable de Cambiar los permisos de un usuario para un álbum determinado.

--- src/main/java/com/prueba/api/wolox/controller/ApiController.java
+++ src/main/java/com/prueba/api/wolox/controller/ApiController.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class ApiController {
@@ -30,11 +32,31 @@ public class ApiController {
        if (!permisoService.findByIdUserCompAndIdUserAndIdAlbum(entity.getIdUserComp(),entity.getIdUser(), entity.getIdAlbum()).isPresent()){
            Permiso entityDB = permisoService.save(entity);
            return ResponseEntity.status(HttpStatus.CREATED).body(entityDB);
-       }else{
-
-           return new ResponseEntity<>("Este ussuario ya posee permisos en este album ", HttpStatus.NOT_FOUND);
        }
+
+           return new ResponseEntity<>("Este usuario ya posee permisos en este album ", HttpStatus.NOT_FOUND);
     }
+
+
+    @PutMapping("/cambiar/album")
+    public ResponseEntity<?> cambiarPermisosPorAlbum(@Valid @RequestBody Permiso permiso, BindingResult result){
+
+        if(result.hasErrors()) {
+            return this.validar(result);
+        }
+        Optional<Permiso> optional =permisoService.findByIdUserCompAndIdUserAndIdAlbum(permiso.getIdUserComp(),permiso.getIdUser(), permiso.getIdAlbum());
+        if (optional==null){
+            Permiso entityDB= optional.get();
+            entityDB.setIdAlbum(permiso.getIdAlbum());
+            entityDB.setIdUser(permiso.getIdUser());
+            entityDB.setIdUserComp(permiso.getIdUserComp());
+            entityDB.setTipoPermiso(permiso.getTipoPermiso());
+            return ResponseEntity.status(HttpStatus.CREATED).body(permisoService.save(entityDB));
+                   }
+        return new ResponseEntity<>("Este usuario ya posee este permiso ", HttpStatus.NOT_FOUND);
+
+    }
+
 
     public ResponseEntity<?> validar(BindingResult result){
         Map<String,Object> errores=  new HashMap<>();
